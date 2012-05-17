a10ef55277cd09d96565e36829ca408e7ac3287f
Kieron Browne <kieron.browne@gmail.com>
2012-05-17 15:26:24 +0000

    Changed title and port for cloud9

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(process.env.C9_PORT, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
